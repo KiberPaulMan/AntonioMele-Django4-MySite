7949cf3fee9a8012b9c363b6cc6ae778f7b73135
Paul <twinencrypt@gmail.com>
2023-05-19 15:52:24 +0700

    Add markdown tag

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
-    # path('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     # path('<int:id>', views.post_detail, name='post_detail'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.share_post, name='post_share'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
 from django.core.mail import send_mail
@@ -6,6 +7,7 @@ from django.views.decorators.http import require_POST
 from blog.models import Post, Comment
 from blog.forms import EmailPostForm, CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -15,24 +17,29 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     # Постраничная разбивка с 3-мя постами на страницу
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         # Если page_number не целое число, то
-#         # выдать первую страницу
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # Если page_number находится вне диапазона, то
-#         # выдать последнюю страницу
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                   template_name='blog/post/list.html',
-#                   context={'posts': posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Постраничная разбивка с 3-мя постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  template_name='blog/post/list.html',
+                  context={'posts': posts,
+                           'tag':  tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -45,11 +52,17 @@ def post_detail(request, year, month, day, post):
                              )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   template_name='blog/post/detail.html',
                   context={'post': post,
                            'comments': comments,
-                           'form': form})
+                           'form': form,
+                           'similar_posts': similar_posts})
 
 
 def share_post(request, post_id):
